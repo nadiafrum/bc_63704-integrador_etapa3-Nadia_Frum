68f8bbf7455f8b5ead5bc08f1abb9ad250b78d51
Nadia Frum <nadiafrum@gmail.com>
2023-11-27 13:04:54 -0300

    detalles

--- config/conectarMongo.js
+++ config/conectarMongo.js
@@ -4,8 +4,8 @@ import mongoose from 'mongoose'
 const conectarMongo = async () => {
 
     try {
-        await mongoose.connect(process.env.URI_LOCAL)
-        //await mongoose.connect(process.env.URI_REMOTA)
+        //await mongoose.connect(process.env.URI_LOCAL)
+        await mongoose.connect(process.env.URI_REMOTA)
         console.log('Conexión a Mongo realizada con éxito!')
     } catch (error) {
         console.log('Error al conectar a Mongo DB', error)
